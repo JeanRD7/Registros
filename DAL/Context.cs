d0540596686f19276a25b5ec5b1238552695a566
JeanRD7 <jean64098@gmail.com>
2023-09-06 13:09:32 -0400

    Creacion del Contexto Cliente

---
+++ DAL/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registros.Models;
+
+namespace Registros.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> Opcions) : base(Opcions) { }
+        public DbSet<Clientes> Clientes { get; set; }
+    }
+}
