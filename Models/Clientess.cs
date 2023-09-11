af9a38f6fa40e93baf64fa35b66599f92f632744
JeanRD7 <jean64098@gmail.com>
2023-09-11 19:23:29 -0400

    Reparando el error de poner la variable ClienteId en singular

---
+++ Models/Clientess.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registros.Models
+{
+    public class Clientes
+    {
+        [Key]
+
+        public int ClienteId { get; set; }
+        [Required (ErrorMessage = "El nombre del cliente es obligatorio")]
+        public string? Nombre { get; set; }
+
+        [Required (ErrorMessage ="El Telegono del cliente es obligatorio")]
+        public string? Telefono { get; set; }
+
+        [Required (ErrorMessage ="El Celular del cliente es obligatoria")]
+        public string? Celular { get; set; }
+
+        [Required (ErrorMessage ="El RNC del cliente es obligatorio")]
+        public string? Rnc { get; set; }
+
+        [Required (ErrorMessage ="El Email del cliente es obligatorio")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "La Direccion del cliente es obligatorio")]
+        public string? Direccion { get; set; }
+    }
+}
